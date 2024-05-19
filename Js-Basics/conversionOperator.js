080867be75e01010815681a78f6f18a8bfc79c03
Devanshibodhare <134762708+Devanshibodhare@users.noreply.github.com>
2024-05-19 15:35:18 +0000

    container config done

---
+++ Js-Basics/conversionOperator.js
@@ -0,0 +1,7 @@
+let score = "t123"
+//console.log(typeof(score)); //datatype -> string
+//typecasting means converting one datatype into another datatype
+
+let ValueNumber = Number(score);
+console.log(typeof(ValueNumber));
+console.log(ValueNumber);  //outpuut is NaN
